a68824258983ffb38e5fe0c7a9d88b1423021e33
ASteinheiser <asteinhe@asu.edu>
2016-02-13 12:49:57 -0700

    finished kit builder mockup

--- src/components/kit/sensor-item.js
+++ src/components/kit/sensor-item.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import Radium from 'radium';
 
+import PrimaryButton from '../buttons/primary-button';
+
 var SensorItem = React.createClass({
   propTypes: {
 
     name: React.PropTypes.string.isRequired,
     description: React.PropTypes.string.isRequired,
+    img: React.PropTypes.string.isRequired,
+    price: React.PropTypes.string.isRequired
   },
 
   render () {
     return (
       <div style={[styles.sensorItemStyle]}>
-        <div style={[styles.sensorNameBox]}>
-          {this.props.name}
+        <div>
+          <img style={[styles.sensorImage]} src={this.props.img}/>
         </div>
-        <div style={[styles.sensorDescriptionBox]}>
-          {this.props.description}
+        <div style={[styles.sensorTitle]}>
+          {this.props.name}
+          <br/>
+          <div style={[styles.sensorDescription]}>
+            {this.props.description}
+          </div>
+        </div>
+        <div style={[styles.sensorPrice]}>
+          Price: {this.props.price}
+          <div style={[styles.addToCartButton]}>
+            <PrimaryButton name='addToCart' type='submit' text='Add To Cart'/>
+          </div>
         </div>
       </div>
     );
@@ -26,35 +40,64 @@ SensorItem = Radium(SensorItem);
 
 var styles = {
   sensorItemStyle: {
+
     borderBottom: '1px solid grey',
 
-    height: '100px',
+    height: '150px',
     width: '100%'
   },
-  sensorNameBox: {
+  sensorImage: {
 
-    fontSize: '24px',
-    fontWeight: 'lighter',
-    fontFamily: 'sans-serif',
-    WebkitFontSmoothing: 'antialiased',
-
-    marginLeft: '10px',
     float: 'left',
-    height: '100px',
+
+    height: '150px',
     width: '200px'
   },
-  sensorDescriptionBox: {
+  sensorTitle: {
 
     fontSize: '24px',
     fontWeight: 'lighter',
     fontFamily: 'sans-serif',
     WebkitFontSmoothing: 'antialiased',
+    color: 'rgb(72, 124, 236)',
 
     paddingLeft: '10px',
     float: 'left',
     height: '100%',
 
     borderLeft: '1px solid grey'
+  },
+  sensorDescription: {
+
+    fontSize: '20px',
+    fontWeight: 'lighter',
+    fontFamily: 'sans-serif',
+    WebkitFontSmoothing: 'antialiased',
+    color: 'black',
+
+    width: '500px',
+    paddingTop: '10px'
+  },
+  sensorPrice: {
+
+    fontSize: '20px',
+    fontWeight: 'lighter',
+    fontFamily: 'sans-serif',
+    WebkitFontSmoothing: 'antialiased',
+    textAlign: 'center',
+
+    float: 'left',
+    borderLeft: '1px solid grey',
+
+    width: '188px',
+    height: '140px',
+    paddingTop: '10px',
+  },
+  addToCartButton: {
+
+    width: '90%',
+
+    margin: '50px auto 0px auto',
   }
 }
 
--- src/pages/kit-builder-page.js
+++ src/pages/kit-builder-page.js
@@ -7,21 +7,10 @@ var KitBuilderPage = React.createClass({
   render () {
     return (
       <LayoutShell>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Motion Sensor' description='this is a motion sensor'/>
-        <SensorItem name='Sound Sensor' description='this is a sound sensor'/>
-        <SensorItem name='Temperature Sensor' description='this is a temperature sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
-        <SensorItem name='Flame Sensor' description='this is a flame sensor'/>
+        <SensorItem img='src/images/flame-sensor.jpg' name='Flame Sensor' description='This sensor uses infrared to detect fires.' price='2.99'/>
+        <SensorItem img='src/images/ir-sensor.jpg' name='Motion Sensor' description='This sensor uses an IR transmitter and reciever to detect motion.' price='2.99'/>
+        <SensorItem img='src/images/sound-sensor.jpg' name='Sound Sensor' description='This sensor detects noise.' price='2.99'/>
+        <SensorItem img='src/images/temperature-sensor.jpg' name='Temperature Sensor' description='This sensor can detect the current temperature.' price='2.99'/>
       </LayoutShell>
     );
   }
