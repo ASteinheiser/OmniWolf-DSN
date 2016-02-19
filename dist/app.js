bd0ad558ab8aa970d31aee50183a1e807a4bc0e5
ASteinheiser <asteinhe@asu.edu>
2016-02-18 21:56:27 -0700

    v1.1

--- dist/app.js
+++ dist/app.js
@@ -26891,7 +26891,7 @@
 	  registeredDeviceStyle: {
 	    WebkitBoxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
 
-	    height: '50%',
+	    height: '380px',
 	    width: '47%',
 	    float: 'left',
 
