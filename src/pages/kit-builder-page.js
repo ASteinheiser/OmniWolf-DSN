90a25a7fc29f53ab842eaff23f4ea4bf72c97c2a
ASteinheiser <asteinhe@asu.edu>
2016-02-18 19:55:41 -0700

    fixed images for displaying form dist

--- src/pages/kit-builder-page.js
+++ src/pages/kit-builder-page.js
@@ -9,27 +9,27 @@ var KitBuilderPage = React.createClass({
     return (
       <LayoutShell>
         <SensorItem
-          img='src/images/flame-sensor.jpg'
+          img='images/flame-sensor.jpg'
           name='Flame Sensor'
           description='This is a digital sensor that uses infrared to detect fires.'
           price='2.99'/>
         <SensorItem
-          img='src/images/ir-sensor.jpg'
+          img='images/ir-sensor.jpg'
           name='Motion Sensor'
           description='This is an digital sensor that uses an IR transmitter and reciever to detect motion.'
           price='2.99'/>
         <SensorItem
-          img='src/images/sound-sensor.jpg'
+          img='images/sound-sensor.jpg'
           name='Sound Sensor'
           description='This is an analog sensor that detects noise.'
           price='2.99'/>
         <SensorItem
-          img='src/images/temperature-sensor.jpg'
+          img='images/temperature-sensor.jpg'
           name='Temperature Sensor'
           description='This is an analog sensor that can detect the current temperature.'
           price='2.99'/>
         <SensorItem
-          img='src/images/carbon-monoxide-sensor.png'
+          img='images/carbon-monoxide-sensor.png'
           name='Carbon Monoxide Sensor'
           description='This is a digital sensor that can notify you of carbon monoxide leaks, great for garages.'
           price='2.99'/>
--- src/pages/pre-assembled-kits-page.js
+++ src/pages/pre-assembled-kits-page.js
@@ -9,27 +9,27 @@ var PreAssembledKitsPage = React.createClass({
     return (
       <LayoutShell>
         <KitItem
-          img='src/images/baby-monitor.jpg'
+          img='images/baby-monitor.jpg'
           name='Baby Monitor'
           description='This is a baby monitor with a fire, motion, and sound sensor. You can always add more sensors later.'
           price='39.99'/>
         <KitItem
-          img='src/images/fire.png'
+          img='images/fire.png'
           name='Fire Detector'
           description='This is a fire detector that can be placed anywhere in your house.'
           price='29.99'/>
         <KitItem
-          img='src/images/motion.png'
+          img='images/motion.png'
           name='Motion Detector'
           description='This is a motion detector that can be placed near a window, pool, or anywhere you want.'
           price='29.99'/>
         <KitItem
-          img='src/images/carbon-monoxide.png'
+          img='images/carbon-monoxide.png'
           name='Carbon Monoxide Detector'
           description='This is a carbon monoxide detector which is great for your garage.'
           price='29.99'/>
         <KitItem
-          img='src/images/noise.png'
+          img='images/noise.png'
           name='Noise'
           description='This is a noise detector which is great for detecting any sounds.'
           price='29.99'/>
