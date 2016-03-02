f49181e7e61983f47fee934bf79ddbb1f14fbe54
ASteinheiser <asteinhe@asu.edu>
2016-03-01 19:03:02 -0700

    fixed top nav

--- src/components/top-nav/index.js
+++ src/components/top-nav/index.js
@@ -4,22 +4,24 @@ import './index.css'
 
 export default class TopNav extends Component {
   render() {
-    return <div className='TopNav--container'>
-      <div className='TopNav--logo'>
-        <a className='no-underline'>OmniWolf</a>
-        <a className='blue no-underline'>DSN</a>
+    return <div className='PageContainer'>
+      <div className='TopNav--container'>
+        <div className='TopNav--logo'>
+          <a className='cursor-pointer'>OmniWolf</a>
+          <a className='blue cursor-pointer'>DSN</a>
+        </div>
+
+        <div className='TopNav--tab-container'>
+          <a className='TopNav--tab'>Pre-Assembled Kits</a>
+          <a className='TopNav--tab'>Kit Builder</a>
+          <a className='TopNav--tab'>Dashboard</a>
+          <a className='TopNav--tab'>Account</a>
+          <a className='TopNav--tab'>Logout</a>
+        </div>
+
+        {this.props.children}
+
       </div>
-
-      <div className='TopNav--tab-container'>
-        <a className='TopNav--tab'>Pre-Assembled Kits</a>
-        <a className='TopNav--tab'>Kit Builder</a>
-        <a className='TopNav--tab'>Dashboard</a>
-        <a className='TopNav--tab'>Account</a>
-        <a className='TopNav--tab'>Log Out</a>
-      </div>
-
-      {this.props.children}
-
     </div>
   }
 }
