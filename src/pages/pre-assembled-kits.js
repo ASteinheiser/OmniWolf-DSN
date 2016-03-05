5c56869a7324454bacf29809b2f3b41c953835e4
ASteinheiser <asteinhe@asu.edu>
2016-03-04 17:03:13 -0700

    added images to express

--- src/pages/pre-assembled-kits.js
+++ src/pages/pre-assembled-kits.js
@@ -15,31 +15,31 @@ export default class PreAssembledKits extends Component {
         link='/kit-builder'
       />
       <StoreItem
-        img='images/baby-monitor.jpg'
+        img='/images/baby-monitor.jpg'
         name='Baby Monitor'
         description='This pre-assembled kit is a basic baby monitor. It includes three sensors: a sound sensor, motion sensor, and flame sensor.'
         price='39.99'
       />
       <StoreItem
-        img='images/fire.png'
+        img='/images/fire.png'
         name='Fire Detector'
         description='This is a fire detector that can be placed anywhere in your house.'
         price='29.99'
       />
       <StoreItem
-        img='images/motion.png'
+        img='/images/motion.png'
         name='Motion Detector'
         description='This is a motion detector that can be placed near a window, pool, or anywhere you want.'
         price='29.99'
       />
       <StoreItem
-        img='images/carbon-monoxide.png'
+        img='/images/carbon-monoxide.png'
         name='Carbon Monoxide Detector'
         description='This is a carbon monoxide detector which is great for your garage.'
         price='29.99'
       />
       <StoreItem
-        img='images/noise.png'
+        img='/images/noise.png'
         name='Noise Detector'
         description='This is a noise detector which is great for windows or anywhere you do not want noise.'
         price='29.99'
