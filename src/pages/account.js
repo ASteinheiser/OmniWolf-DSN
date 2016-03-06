5f6a27a8d40a311edb076d59241578827cd27362
ASteinheiser <asteinhe@asu.edu>
2016-03-06 15:17:52 -0700

    added profile card

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
 import PaperContainer from '../components/paper-container'
+import ProfileCard from '../components/profile-card'
 
 export default class Account extends Component {
   render() {
     return <PaperContainer>
+      <ProfileCard />
     </PaperContainer>
   }
 }
