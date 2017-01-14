6ebd83e99993bc196069bb1f4efbed1f6137db59
ASteinheiser <asteinhe@asu.edu>
2017-01-14 10:43:47 -0700

    added add action button

---
+++ src/components/add-action/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import StyledText from '../styled-text'
+
+import styles from './styles.css'
+
+export default class AddAction extends Component {
+  render() {
+    return (
+      <div className={[styles.container]}>
+        <div className={[styles.padding]}>
+
+          <StyledText
+            text="Add Action"
+            size="large"
+            color="blue"
+          />
+
+        </div>
+      </div>
+    )
+  }
+}
--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 
 import StyledText from '../components/styled-text'
 import SensorDisplay from '../components/sensor-display'
+import AddAction from '../components/add-action'
 
 import styles from './styles.css'
 
@@ -11,9 +12,11 @@ export default class Dashboard extends React.Component {
       <div className={[styles.spaceTop]}>
 
         <SensorDisplay title="Font Door">
+          <AddAction />
         </SensorDisplay>
 
         <SensorDisplay title="Baby's Room">
+          <AddAction />
         </SensorDisplay>
 
       </div>
