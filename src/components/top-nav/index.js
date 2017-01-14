e75f9d010670f0dccccf0624e40e8b6d34d3c318
ASteinheiser <asteinhe@asu.edu>
2017-01-14 03:46:01 -0700

    added omniwolf logo

--- src/components/top-nav/index.js
+++ src/components/top-nav/index.js
@@ -11,9 +11,11 @@ export default class TopNav extends React.Component {
 
   render() {
     return (
-      <div className={[styles.bodyWidth]}>
+      <div>
 
         <div className={[styles.topNavContainer]}>
+
+          <img className={[styles.logo]} src="https://s3-us-west-2.amazonaws.com/iamandrew.io-images/logo.svg"/>
 
         </div>
 
