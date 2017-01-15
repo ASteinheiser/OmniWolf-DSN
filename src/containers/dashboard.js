fcc479a8fb842957b8109d586f5bc0f3fa1156e8
ASteinheiser <asteinhe@asu.edu>
2017-01-14 20:56:27 -0700

    finished gauges

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react'
+import 'whatwg-fetch'
+import Gauge from 'react-svg-gauge'
 
 import StyledText from '../components/styled-text'
 import SensorDisplay from '../components/sensor-display'
@@ -9,6 +11,28 @@ import Search from '../components/search'
 import styles from './styles.css'
 
 export default class Dashboard extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { fire: false }
+    this.state = { motion: false }
+    this.state = { temp: 0 }
+  }
+
+  componentDidMount() {
+    let self = this
+    setInterval(function() {
+      fetch("http://api.omniwolf.io/device/pull", {"mode": "cors"}).then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        self.setState({ temp: Math.round(json.Items[0].Data * 10) / 10 })
+        self.setState({ motion: 1 - json.Items[1].Data })
+        self.setState({ fire: 1 - json.Items[2].Data })
+      })
+    }, 1500)
+  }
+
   render() {
     return (
       <div className={[styles.spaceTop]}>
@@ -35,16 +59,26 @@ export default class Dashboard extends React.Component {
 
         </div>
 
-        <SensorDisplay title="Font Door">
-          <AddAction />
+        <SensorDisplay title="Master Bedroom">
+          <span>
+
+            <Gauge value={this.state.temp} width={250} height={150} color={'rgb(72, 124, 236)'} backgroundColor={'rgb(215,215,215)'} label="Temp(F)" />
+
+            <Gauge value={this.state.fire} width={250} height={150} max={1} color={'rgb(72, 124, 236)'} backgroundColor={'rgb(215,215,215)'} label="Fire" />
+
+            <AddAction />
+
+          </span>
         </SensorDisplay>
 
-        <SensorDisplay title="Baby's Room">
-          <AddAction />
-        </SensorDisplay>
+        <SensorDisplay title="Front Door">
+          <span>
 
-        <SensorDisplay title="Back Window">
-          <AddAction />
+            <Gauge value={this.state.motion} width={250} height={150} max={1} color={'rgb(72, 124, 236)'} backgroundColor={'rgb(215,215,215)'} label="Motion" />
+
+            <AddAction />
+
+          </span>
         </SensorDisplay>
 
         <div className={[styles.bottomSpace]} />
